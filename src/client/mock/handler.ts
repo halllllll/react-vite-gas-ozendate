de6338f1c8d0acf399d90dc6e35b69fa1441aebe
h4lllllll <hjod1172@yahoo.co.jp>
2025-07-02 23:01:51 +0000

    refactor: update handlersRecord to use ApiEndpoint type for improved type safety

--- src/client/mock/handler.ts
+++ src/client/mock/handler.ts
@@ -1,10 +1,9 @@
-import type { API_ENDPOINT } from '@/client/api/apiEndpoint';
 import type { SheetNameResponse, SheetUrlResponse } from '@/shared/types/sheet';
 import { delay, http, HttpResponse } from 'msw';
-import { getApiPath } from '../api/endpoint';
+import { type ApiEndpoint, getApiPath } from '../api/endpoint';
 import { MOCK_SHEET_NAME, MOCK_SHEET_URL } from './data';
 
-const handlersRecord: Record<keyof typeof API_ENDPOINT, any> = {
+const handlersRecord: Record<ApiEndpoint, any> = {
   SHEET_NAME: http.get<never, never, SheetNameResponse>(getApiPath('SHEET_NAME'), async () => {
     console.info('--- mock api: sheet name ---');
     await delay(400);
